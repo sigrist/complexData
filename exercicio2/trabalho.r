862bd4da762e314bfdf12e4f44c61512873f2e4a
sigrist <paulo.sigrist@gmail.com>
2015-10-11 15:54:23 -0300

    Finalizado relatorio

--- exercicio2/trabalho.r
+++ exercicio2/trabalho.r
@@ -6,22 +6,20 @@
 # Returns:
 #  Data frame com medidas de temperatura obtidas do cepagri
 loadAndFilterData <- function() {
-  
   con <- url("http://www.ic.unicamp.br/~zanoni/cepagri/cepagri.csv") 
   cpa <- read.csv(con, header = FALSE, sep = ";",col.names = c("horario","temperatura","vento","umidade","sensacao"), as.is = TRUE)  
   
   # Transforma horario em formato de data para facilitar manipulacao
   cpa[["horario"]] <- strptime(cpa[["horario"]], "%d/%m/%Y-%H:%M")
 
-  cpa <- subset(cpa, cpa$horario >= as.POSIXlt("2014-07-01") & cpa$horario <= as.POSIXlt("2015-06-30 23:59:59"))
+  # Filtrar o data frame pela data estipulda
+  cpa <- cpa[cpa$horario >= as.POSIXlt("2014-07-01 00:00:00") & cpa$horario <= as.POSIXlt("2015-06-30 23:59:59"),]
   
   # Converter colunas strings para numerica
   cpa[["temperatura"]] <- as.double(cpa[["temperatura"]])
   cpa[["vento"]] <- as.double(cpa[["vento"]])
   cpa[["umidade"]] <- as.double(cpa[["umidade"]])
   cpa[["sensacao"]] <- as.double(cpa[["sensacao"]])
-  
-  
   
   return(cpa)
 }
@@ -39,49 +37,89 @@ meanByMonth <- function(cpa) {
   
 }
 
+# Retorna todos os dados de um mes especifico
 dataByMonth <- function(m, cpa) {
-  subset(cpa, cpa$horario$mon == m)
+  cpa[cpa$horario$mon == m,]
+}
+
+# Retorna todos os dados de um periodo
+dataByInterval <- function(from, to, cpa) {
+  cpa[cpa$horario >= as.POSIXlt(from) & cpa$horario <= as.POSIXlt(to),]
 }
 
 # Retorna a media de temperatura por dia de um CPA
-meanByDay <- function(cpa) {
-  tapply(cpa[["temperatura"]], cpa[["horario"]]$mday, mean, na.rm = TRUE)
+meanByDay <- function(month, cpa) {
+  x <- dataByMonth(month, cpa)
+  tapply(x[["temperatura"]], x[["horario"]]$mday, mean, na.rm = TRUE)
 }
 
 temperatureByDay <- function(cpa) {
+  # Aplicar a funcao meanByDay para os meses de 0-11 no data frame cpa
+  r <- sapply(c(0:11), meanByDay, cpa)
   
-  # TODO COmo fazer isso com um comando (apply, tapply??)
-  jan <- meanByDay(dataByMonth(0, cpa))
-  fev <- meanByDay(dataByMonth(1, cpa))
-  mar <- meanByDay(dataByMonth(2, cpa))
-  abr <- meanByDay(dataByMonth(3, cpa))
-  mai <- meanByDay(dataByMonth(4, cpa))
-  jun <- meanByDay(dataByMonth(5, cpa))
-  jul <- meanByDay(dataByMonth(6, cpa))
-  ago <- meanByDay(dataByMonth(7, cpa))
-  set <- meanByDay(dataByMonth(8, cpa))
-  out <- meanByDay(dataByMonth(9, cpa))
-  nov <- meanByDay(dataByMonth(10, cpa))
-  dez <- meanByDay(dataByMonth(11, cpa))
-  
+  # Vetor de cores para serem utilizadas no grafico
+  cores <- c("red", "blue", "green", "yellow", "brown", "purple", "yellowgreen", "orange", "magenta", "pink", "navy", "black")
+
+    # Plotar um grafico  
   plot(c(10:32),
        type = "n",
        main = "Alteracao de temperatura por dia",
        xlab = "Dias", ylab = "Graus (em Celsius)")
-  lines(jan, col = "red")
-  lines(fev, col = "blue")
-  lines(mar, col = "green")
-  lines(abr, col = "yellow")
-  lines(mai, col = "brown")
-  lines(jun, col = "purple")
-  lines(jul, col = "yellowgreen")
-  lines(ago, col = "orange")
-  lines(set, col = "magenta")
-  lines(out, col = "pink")
-  lines(nov, col = "navy")
-  lines(dez, col = "black")
+  
+  # Para cada resultado, adicionar uma linha ao grafico
+  sapply(c(1:12), function (l) { lines(unlist(r[l]), col=cores[l]) })
   
   legend("bottomleft", month.name,
-         col = c("red", "blue", "green", "yellow", "brown", "purple", "yellowgreen", "orange", "magenta", "pink", "navy", "black"), pch = 15, cex = 0.75)
+         col = cores, pch = 15, cex = 0.75)
 
+}
+
+# Cria uma estrutura que divide os dados pela estacao do ano
+# acessar como:
+#   m$inverno 
+#   m$outono 
+#   m$verao 
+#   m$primavera 
+
+dataBySeason <- function(cpa) {
+  # Estacoes do ano
+  #   - 21 Junho 2014 - Inverno
+  #   - 22 Setembro 2014 - Primavera
+  #   - 21 Dezembro 2014 - Verao
+  #   - 20 Marco 2015 - Outono
+  #   - 21 Junho 2014 - Inverno
+  
+  
+  inverno <- dataByInterval("2014-06-21 00:00:00", "2014-09-21 23:59:59", cpa)
+  primavera <- dataByInterval("2014-09-22 00:00:00", "2014-12-20 23:59:59", cpa)
+  verao <- dataByInterval("2014-12-22 00:00:00", "2015-03-19 23:59:59", cpa)
+  outono <- dataByInterval("2015-03-20 00:00:00", "2015-06-20 23:59:59", cpa)
+  inverno2 <- dataByInterval("2015-06-21 00:00:00", "2015-09-21 23:59:59", cpa)
+  # Concatenar os dois periodos de inverno
+  inverno <- rbind(inverno, inverno2)
+  
+  m <- 0
+  m$inverno <- inverno
+  m$primavera <- primavera
+  m$verao <- verao
+  m$outono <- outono
+  
+  return(m)
+}
+
+# Cria um histograma com os dados
+histogramByValue <- function(data, title, xlabel = title, col1 = "rosybrown", col2 = "blue") {
+  h <- hist(data, main = title,
+            xlab = xlabel,
+            ylab = "Ocorrencias", col = col1)
+  xfit <- seq(min(data, na.rm = TRUE), max(data, na.rm = TRUE), length = 100)
+  yfit <- dnorm(xfit, mean = mean(data, na.rm = TRUE), sd = sd(data, na.rm = TRUE))
+  yfit <- yfit * diff(h$mids[1:2]) * length(data)
+  lines(xfit, yfit, col = col2, lwd = 3)
+}
+
+stripChartAll <- function(data, title) {
+  
+  boxplot(data[,2:5], main = title,
+          col = rgb(200, 0, 0, 100, maxColorValue = 255))
 }
